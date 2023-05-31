577027c447a29292fbf848c074367e15485e3bba
Ronal Ramos <ronalirv23@gmail.com>
2023-05-31 16:46:16 -0500

    laravel-react

---
+++ resources/js/Pages/Inicio.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import MainLayout from "../Layouts/Plantilla";
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <h1 className="text-3xl">Hello world!</h1>
+    </MainLayout>
+  );
+};
+
+export default HomePage;
+
