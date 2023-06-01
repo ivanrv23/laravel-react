6cca5a9fe825eb61ad46198ac565e4a322ee4269
Ronal Ramos <ronalirv23@gmail.com>
2023-05-31 21:39:12 -0500

    plantilla - header - footer

--- resources/js/Layouts/Plantilla.jsx
+++ resources/js/Layouts/Plantilla.jsx
@@ -1,39 +1,14 @@
-
-import React from "react";
-
-const MainLayout = ({ contenido }) => {
-  return (
-    <div className="flex flex-col h-screen">
-      <header className="w-full px-6 py-4 bg-gray-800">
-        <nav className="flex items-center justify-between">
-          <div className="flex items-center">
-            <a href="/" className="text-lg font-bold text-white">
-              Logo
-            </a>
-          </div>
-
-          <div>
-            <ul className="flex items-center justify-end text-white">
-              <li className="mr-6">
-                <a href="#">Link 1</a>
-              </li>
-              <li className="mr-6">
-                <a href="#">Link 2</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-
-      <main className="flex-1 bg-gray-100">
-        <div className="container px-4 mx-auto">{contenido}</div>
-      </main>
-
-      <footer className="w-full px-6 py-4 text-white bg-gray-800">
-        <p>Copyright © {new Date().getFullYear()}</p>
-      </footer>
-    </div>
-  );
-};
-
-export default MainLayout;
+import HeaderPrincipal from '@/Components/Header';
+import FooterPrincipal from '@/Components/Footer';
+export default function MainLayout({children}) {
+    return (
+        <div className="flex flex-col h-screen">
+            {/* header */}
+            <HeaderPrincipal/>
+            {/* Content */}
+            <main className="container mx-auto mt-28">{children}</main>
+            {/* Footer */}
+            <FooterPrincipal/>
+        </div>
+    );
+}
--- resources/js/Pages/Inicio.jsx
+++ resources/js/Pages/Inicio.jsx
@@ -1,14 +1,9 @@
+import MainLayout from "@/Layouts/Plantilla";
 
-import React from "react";
-import MainLayout from "../Layouts/Plantilla";
-
-const HomePage = () => {
-  return (
-    <MainLayout>
-      <h1 className="text-3xl">Hello world!</h1>
-    </MainLayout>
-  );
-};
-
-export default HomePage;
-
+export default function HomePage() {
+    return (
+        <MainLayout>
+            <h1 className="text-3xl">Hello world!</h1>
+        </MainLayout>
+    );
+}
