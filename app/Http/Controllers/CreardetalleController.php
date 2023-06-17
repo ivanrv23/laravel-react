833988c95fddba43628993b608e4692bc9a42d95
Dennis Pérez Tapia <dennis_murset@hotmail.com>
2023-06-17 00:14:36 -0500

    asd

---
+++ app/Http/Controllers/CreardetalleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\creardetalle;
+use App\Http\Requests\StorecreardetalleRequest;
+use App\Http\Requests\UpdatecreardetalleRequest;
+
+class CreardetalleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorecreardetalleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(creardetalle $creardetalle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(creardetalle $creardetalle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatecreardetalleRequest $request, creardetalle $creardetalle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(creardetalle $creardetalle)
+    {
+        //
+    }
+}
